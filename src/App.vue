346d8209e0bb98a0f75c53fd9e80f1ab02c048d9
Nmarcaud <n.marcaud@patriarche.fr>
2023-04-26 15:12:35 +0200

    Module 4 - Fin

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <Navbar />
   <main class="container" style="margin-top: 70px">
     <h1>Ponyracer</h1>
+    <Races />
   </main>
 </template>
 
 <script lang="ts" setup>
 import Navbar from './components/Navbar.vue';
+import Races from '@/views/Races.vue'
 </script>
 
 <style></style>
---
+++ src/components/Race.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>{{ raceModel.name }}</h2>
+  <ul v-for="pony in raceModel.ponies" :key="pony.id">
+    <li>{{ pony.name }}</li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps(["raceModel"]);
+</script>
+
+<style>
+</style>
